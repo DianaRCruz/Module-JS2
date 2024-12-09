a4f8b3368a03fad80ea51eb62560470e50506b9b
Diana Cruz <158202602+DianaRCruz@users.noreply.github.com>
2024-12-10 00:15:49 +0100

    rey1

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,4 +1,59 @@
-function setAlarm() {}
+// need to get the input
+
+const timeInput = document.getElementById("alarmSet");
+const timeOutput = document.getElementById("timeRemaining");
+console.log(timeInput.value);
+
+function setAlarm() {
+  console.log(timeInput.value);
+  var minutes = Math.trunc(timeInput.value / 60);
+  var seconds = Math.trunc(timeInput.value % 60);
+  console.log(minutes);
+  console.log(seconds);
+  let stringMinutes;
+  let stringSeconds;
+
+  if (minutes < 10) {
+    stringMinutes = `0${minutes}`;
+  } else {
+    stringMinutes = `${minutes}`;
+  }
+  if (seconds < 9) {
+    stringSeconds = `0${seconds}`;
+  } else {
+    stringSeconds = `${seconds}`;
+  }
+
+  timeOutput.innerText = `Time remaining: ${stringMinutes}:${stringSeconds}`;
+  return { minutes, seconds };
+}
+
+let globalMinutes, globalSeconds;
+
+function updateGlobalVariables() {
+  const time = setAlarm(); // Call the function and get the returned object
+  globalMinutes = time.minutes;
+  globalSeconds = time.seconds;
+
+  console.log("Global variables updated:", globalMinutes, globalSeconds);
+}
+
+function startCountdown(sec, min) {
+  while (sec > 0)
+    while (sec > 0) {
+      sec -= 1;
+
+      setInterval(() => {
+        setAlarm();
+      }, 1000);
+    }
+
+  if (min > 0) {
+    sec = +60;
+
+    min = -1;
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -7,6 +62,7 @@ var audio = new Audio("alarmsound.mp3");
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
+    startCountdown(globalSeconds, globalMinutes);
   });
 
   document.getElementById("stop").addEventListener("click", () => {
